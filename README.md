e4b7a6f01a502ecacd800704c527acb11b97ef0c
Lee Sean <leesean18082001@gmail.com>
2024-05-19 21:48:33 +0800

    updates to eda, main and README.md

--- README.md
+++ README.md
@@ -1,1 +1,110 @@
-# aiip3-lee-sean-807E
+
+# AIIP 3 Techinical Assessment
+Name: Lee Sean \
+Email: leesean18082001@gmail.com
+## Project Directory
+```bash
+├── .github
+├── src
+│   ├── main.py
+├── .env
+├── .gitignore
+├── eda.ipynb
+├── README.md
+├── requirements.txt
+└── run.sh
+```
+## Running Pipeline
+```bash
+pip install -r requirements.txt
+./run.sh
+```
+## Modifying Environment Variables
+The .env file contains the variables to be configured to change things within the pipeline.
+
+- For CATEGORICAL_FEATURES, it indicates the features that are categorical in nature to be used in the model. The default configurations to choose from are: 
+Flagged by Carrier,Is International,Country Prefix,Call Type,Year,Month,Day,Hour,Device Battery
+
+- For NUMERIC_FEATURES, it indicates the features that are numerical in nature to be used in the model. The default configurations to choose from are: 
+ID,Call Duration,Call Frequency,Financial Loss,Previous Contact Count 
+
+- For CATEGORICAL_FEATURES and NUMERIC_FEATURES please ensure that there are no spaces between the commas. Multiple features can be chosen for these two variables.
+
+- For CATEGORICAL_IMPUTER_STRATEGY, it indicates the strategy to be used to handle null data entries for columns with data that are categorical in nature. The default configurations to choose from are:
+mean / median / most_frequent / 0
+
+- For NUMERIC_IMPUTER_STRATEGY, it indicates the strategy to be used to handle null data entries for columns with data that are numerical in nature. The default configurations to choose from are:
+mean / median / most_frequent / 0
+
+- For ONEHOT_HANDLE_UNKNOWN, it indicates the strategy to be used to handle categories that were not seen in the training data. The default configurations to choose from are:
+error / ignore
+
+- For CATEGORICAL_IMPUTER_STRATEGY, NUMERIC_IMPUTER_STRATEGY and ONEHOT_HANDLE_UNKNOWN, only one option should be used. 
+
+- For TEST_SIZE, it indicates the ratio between the test size and the total size of the data set. The value should be a number between 0 and 1. The default should be left at 0.2.
+
+- For NUMBER_OF_RUNS, it indicates the number of times the pipeline should be ran. The return result of the pipeline would be the average of the number of runs specified here. The default should be left at 10.
+
+- For ALGORITHM, it indicates the algorithm used for building the model. The default configurations to choose from are:
+ RandomForest / GradientBoosting / DecisionTree \
+ Only one option should be chosen.
+ 
+- If RandomForest is chosen, you may choose to update:
+    - NUMBER_OF_TREES, which indicates the number of decision trees grown to train the model. The default should be left at 100.
+
+- If GradientBoosting is chosen, you may choose to update:
+    - GRADIENT_NUMBER_OF_DECISIONS_TREES, which indicates the number of decision trees grown to train the model. The default should be left at 100.
+
+    - GRADIENT_LEARNING_RATE, which indicates the amount of contribution that each new tree has for the models overall prediction. The default should be left at 0.1.
+## Pipeline Logic
+The overarching pipeline consists of a preprocessing step followed by the classifier. This passes the cleaned and processed data into the machine learning model to generate a model to predict whether the call is a scam.This can be seen in the image below:\
+\
+The preprocessing step is a column transformer which helps to handle potential errors in the dataset. This column transformer contains two different pipelines, the categorical processor and the numerical processor, to handle the categorical column and the numerical column respectively. This can be seen in the image below:\
+\
+The categorical processor contains two steps, SimpleImputer and OneHotencoder. The SimpleImputer handles missing/null values in the column, and applies the chosen strategy to it, while the OneHotEncoder handles unkown categories that were not seen in the training set, and applies the chosen strategy to it. This can be seen in the image below:\
+\
+The numerical processor contains two steps as well, SimpleImputer and StandardScaler. The SimpleImputer handles missing/null values in the column, and applies the chosen strategy to it, while the StandardScaler normalizes the data, so that everything is properly scaled. This can be seen in the image below:\
+\
+Before the data is even passed into either the numerical or categorical processor, it also need to be cleaned and transformed. This step includes:
+- Removing non_unique IDS
+- Transform timestamp into Year, Month, Day and Hour.
+- Change Call Type column data from Whats App to Whatsapp.
+After all the data is processed, the data is split into training set and test set randomly, and the model is trained using the training set.\
+\
+Once trained, the model is fed the test set, to try and predict the target, which in this is case is whether or not the call is a scam.\
+\
+The final step in the pipeline is evaluating the performance of the model using the following performance metrics: accuracy, precision and confusion matrix. This is printed to the console for the user to review.
+## Overview of EDA 
+- There is a repeated Call Type, Whats App and Whatsapp
+- Financial Loss contains 1403 null entries
+- ID contains 2000 non-unique entries
+- Financial Loss and Call Duration contains 232 and 2500 negative values respectively.
+- Timestamp needs to be converted into a more interpretable data type, which is Year, Month, Day, Hour.
+- ID should not be used as a feature because it is just a unique identifier to identify the call.
+- Data is inconclusive on whether or not Financial Loss has a correlation with scam calls.
+- Data is conclusive that Call Duration has no correlation with scam calls.
+- Data is inconclusive on whether or not Call Frequency has a correlation with scam calls.
+- Data is conclusive that Flagged By Carrier has correlation with scam calls.
+- Data is conclusive that Is International has no correlation with scam calls.
+- Data is conclusive that Previous Contact Count has no correlation with scam calls.
+- Data is conclusive that Country Prefix has a correlation with scam calls.
+- Data is conclusive that Call type has correlation with scam calls.
+- Data is conclusive that Year has no correlation with scam calls.
+- Data is conclusive that Month has no correlation with scam calls.
+- Data is conclusive that Day has no correlation with scam calls.
+- Data is conclusive that Hour has a correlation with scam calls.
+- Data is conclusive that Battery Device has no correlation with scam calls.
+## Processing of Datasets
+|Categorical|Numerical|
+|-|-|
+|SimpleImputer|SimpleImputer|
+|OneHotEncoder|StandardScaler|
+
+## Choice of models
+Based on the problem statement, the task at hand is a supervised classification task. This is because we are given a labelled dataset (whether or not the call is a scam) and we are tasked to create a model that can classify each call. Because of this, the choice of models are as shown below:
+- Decision Tree
+    - Due to high dimensionality of the data, 
+- Gradient Boosting
+    - 
+- Random Forest
+    - 
--- src/main.py
+++ src/main.py
@@ -1,64 +1,131 @@
+import pandas as pd
+import numpy as np
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-import pandas as pd
-import numpy as np
-from sqlalchemy import create_engine
-import os
-from dotenv import load_dotenv
+from sklearn.metrics import accuracy_score, precision_score, confusion_matrix
 
 load_dotenv()
-def parse_env_list(env_var):
+
+def parse_env_list(env_var: str) -> list:
     return os.getenv(env_var, "").split(",")
-os.environ['PYTHON_ENV'] = 'development'
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-db_path = os.path.join(base_dir, '../data/calls.db')
-engine = create_engine(f'sqlite:///{db_path}')
-df = pd.read_sql_query("SELECT * FROM calls", engine)
+class DataProcessor:
+    def __init__(self, db_path: str):
+        self.db_path = db_path
+        self.engine = create_engine(f'sqlite:///{db_path}')
+    
+    def load_data(self) -> pd.DataFrame:
+            df = pd.read_sql_query("SELECT * FROM calls", self.engine)
+            return df
+    
+    def preprocess_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        non_unique_ids = df['ID'].value_counts()[df['ID'].value_counts() > 1].index
+        non_unique_df = df[df['ID'].isin(non_unique_ids)].drop_duplicates(subset='ID', keep='first')
+        df = pd.concat([df[~df['ID'].isin(non_unique_ids)], non_unique_df])
+        df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+        df['Year'] = df['Timestamp'].dt.year
+        df['Month'] = df['Timestamp'].dt.month
+        df['Day'] = df['Timestamp'].dt.day
+        df['Hour'] = df['Timestamp'].dt.hour
+        df['Call Type'] = df['Call Type'].replace('Whats App', 'WhatsApp')
+        return df
 
-categorical_features = parse_env_list("CATEGORICAL_FEATURES")
-numeric_features = parse_env_list("NUMERIC_FEATURES")
+class ModelPipeline:
+    def __init__(self, categorical_features: list, numeric_features: list, algorithm: str):
+        self.categorical_features = categorical_features
+        self.numeric_features = numeric_features
+        self.algorithm = self._select_algorithm(algorithm)
+        self.pipeline = self._create_pipeline()
+    
+    def _select_algorithm(self, algorithm_name: str):
+        if algorithm_name == "GradientBoosting":
+            return GradientBoostingClassifier(
+                n_estimators=int(os.getenv("GRADIENT_NUMBER_OF_DECISIONS_TREES")),
+                learning_rate=float(os.getenv("GRADIENT_LEARNING_RATE"))
+            )
+        elif algorithm_name == "RandomForest":
+            return RandomForestClassifier(n_estimators=int(os.getenv("NUMBER_OF_TREES")))
+        else:
+            return DecisionTreeClassifier()
+    
+    def _create_pipeline(self) -> Pipeline:
+        categorical_processor = Pipeline(
+            steps=[
+                ("imputation_error", SimpleImputer(strategy=os.getenv("CATEGORICAL_IMPUTER_STRATEGY", "most_frequent"))),
+                ("onehot", OneHotEncoder(handle_unknown=os.getenv("ONEHOT_HANDLE_UNKOWN", "ignore")))
+            ]
+        )
+        numeric_processor = Pipeline(
+            steps=[
+                ("imputation_constant", SimpleImputer(
+                    missing_values=np.nan,
+                    strategy="constant" if os.getenv("NUMERIC_IMPUTER_STRATEGY") == "0" else os.getenv("NUMERIC_IMPUTER_STRATEGY"),
+                    fill_value=0
+                )),
+                ("scaler", StandardScaler())
+            ]
+        )
+        preprocessor = ColumnTransformer(
+            [
+                ("categorical", categorical_processor, self.categorical_features),
+                ("numeric", numeric_processor, self.numeric_features)
+            ]
+        )
+        return Pipeline(steps=[('preprocessor', preprocessor), ('classifier', self.algorithm)])
+    
+    def train_and_evaluate(self, X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.Series, y_test: pd.Series, n_runs: int):
+        accuracy_total = 0
+        precision_total = 0
+        conf_matrix_total = np.array([[0, 0], [0, 0]])
+        
+        for _ in range(n_runs):
+            self.pipeline.fit(X_train, y_train)
+            y_pred = self.pipeline.predict(X_test)
+            accuracy_total += accuracy_score(y_test, y_pred)
+            precision_total += precision_score(y_test, y_pred, pos_label="Scam")
+            conf_matrix_total += confusion_matrix(y_test, y_pred, labels=["Scam", "Not Scam"])
+        
+        accuracy_avg = accuracy_total / n_runs
+        precision_avg = precision_total / n_runs
+        conf_matrix_avg = conf_matrix_total // n_runs
+        
+        return accuracy_avg, precision_avg, conf_matrix_avg
 
-non_unique_ids = df['ID'].value_counts()[df['ID'].value_counts() > 1].index
-non_unique_df = df[df['ID'].isin(non_unique_ids)]
-non_unique_df = non_unique_df.drop_duplicates(subset='ID', keep='first')
-df = pd.concat([df[~df['ID'].isin(non_unique_ids)], non_unique_df])
+def main():
+    os.environ['PYTHON_ENV'] = 'development'
+    
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(base_dir, '../data/calls.db')
+    
+    processor = DataProcessor(db_path)
+    df = processor.load_data()
+    df = processor.preprocess_data(df)
+    
+    X = df.drop(columns=["Scam Call"])
+    y = df["Scam Call"]
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=float(os.getenv("TEST_SIZE")), random_state=42)
+    
+    categorical_features = parse_env_list("CATEGORICAL_FEATURES")
+    numeric_features = parse_env_list("NUMERIC_FEATURES")
+    
+    model_pipeline = ModelPipeline(categorical_features, numeric_features, os.getenv("ALGORITHM"))
+    accuracy_avg, precision_avg, conf_matrix_avg = model_pipeline.train_and_evaluate(X_train, X_test, y_train, y_test, int(os.getenv("NUMBER_OF_RUNS")))
+    
+    print("Average accuracy of the model with the provided configurations is: {:.2f}%".format(accuracy_avg * 100))
+    print("Average precision of the model with the provided configurations is: {:.2f}%".format(precision_avg * 100))
+    print("Confusion Matrix of the model with the provided configurations is as follows:")
+    print("                    Actual Scam   Actual Not Scam")
+    print("Predicted Scam          {}            {}".format(conf_matrix_avg[0][0], conf_matrix_avg[0][1]))
+    print("Predicted Not Scam      {}            {}".format(conf_matrix_avg[1][0], conf_matrix_avg[1][1]))
 
-X = df.drop(columns=["Scam Call"])
-y = df["Scam Call"]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=float(os.getenv("TEST_SIZE")), random_state=42)
-
-categorical_processor = Pipeline(
-    steps=[
-        ("imputation_error", SimpleImputer(strategy=os.getenv("CATEGORICAL_IMPUTER_STRATEGY", "most_frequent"))),
-        ("onehot", OneHotEncoder(handle_unknown=os.getenv("ONEHOT_HANDLE_UNKOWN", "ignore")))
-    ]
-)
-numeric_processor = Pipeline(
-    steps=[
-        ("imputation_constant", SimpleImputer(missing_values=np.nan, strategy="constant" if os.getenv("NUMERIC_IMPUTER_STRATEGY") == "0" else os.getenv("NUMERIC_IMPUTER_STRATEGY"), fill_value=0)),
-        ("scaler", StandardScaler())
-    ]
-)
-preprocessor = ColumnTransformer(
-    [
-        ("categorical", categorical_processor, categorical_features),
-        ("numeric", numeric_processor, numeric_features)
-    ]
-)
-pipeline_steps = [
-    ('preprocessor', preprocessor),
-    ('classifier', LogisticRegression(max_iter=1000))
-]
-
-pipeline = Pipeline(pipeline_steps)
-pipeline.fit(X_train, y_train)
-y_pred = pipeline.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy of the model with the provided configurations is: {:.2f}%".format(accuracy * 100))
+if __name__ == "__main__":
+    main()
